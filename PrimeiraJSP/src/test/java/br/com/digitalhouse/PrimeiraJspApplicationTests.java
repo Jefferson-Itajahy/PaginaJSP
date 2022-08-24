9602cdc91bf1ae23cb431f5b353c1cc3821a5568
Jefferson <itajahy@gmail.com>
2022-08-24 10:29:47 -0300

    Primeira página JSP com Spring Boot[C

---
+++ PrimeiraJSP/src/test/java/br/com/digitalhouse/PrimeiraJspApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
